6271f39bbfb917c725823dd6df81ae1d8e12590f
williamlizarazo <william.lizarazo@gmail.com>
2026-08-10 09:43:13 -0500

    aporte de William al dashboard

--- archivos_requeridos/app.py
+++ archivos_requeridos/app.py
@@ -13,7 +13,7 @@ app = dash.Dash(
     __name__,
     meta_tags=[{"name": "viewport", "content": "width=device-width, initial-scale=1"}],
 )
-app.title = "Dashboard energia"
+app.title = "Dashboard energia - aporte William"
 
 server = app.server
 app.config.suppress_callback_exceptions = True
@@ -244,3 +244,4 @@ def update_output_div(date, hour, proy):
 # Run the server
 if __name__ == "__main__":
     app.run(debug=True)
+
