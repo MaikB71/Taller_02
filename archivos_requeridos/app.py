9dffba8bd0765bd983d182d58d2988d4de4a860c
MaikB71 <maicolbustos_07177@hotmail.com>
2026-08-09 22:48:18 -0500

    mod: pruebas

--- archivos_requeridos/app.py
+++ archivos_requeridos/app.py
@@ -22,7 +22,7 @@ app.config.suppress_callback_exceptions = True
 # Load data from csv
 def load_data():
     data = pd.read_csv("datos_energia.csv")
-    data["time"] = pd.to_datetime(data["time"])
+    data["time"] = pd.to_datetime(data["time"]) #time
     data = data.set_index("time")
     return data
     
